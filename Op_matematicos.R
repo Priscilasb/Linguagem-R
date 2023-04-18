c83195012adfaa5548fdae50874ff3df612f3ddd
Priscila dos Santos Benedito <90283421+Priscilasb@users.noreply.github.com>
2023-04-18 18:00:37 -0300

    Add files via upload

---
+++ Op_matematicos.R
@@ -0,0 +1,77 @@
+####operadores matematicos#####
+
+#SOMA
+5 + 3 
+
+#SUBTRAÇÃO 
+8 - 6
+
+#MULTIPLICAÇÃO 
+4 * 3 
+
+#DIVISÃO
+20 / 5 
+
+#POTENCIAÇÃO 
+4 ** 2 
+
+#OU 
+4 ^ 2 
+
+#RADIAÇÃO
+
+#RAIZ QUADRADA
+sqrt(81)
+81**(1/2)
+
+#RAIZ CÚBICA
+125**(1/3)
+27**(1/3)
+
+#MÓDULO
+abs(10)
+abs(-10)
+
+#FATORIAL
+factorial(3)
+factorial(5)
+
+#LOGARITMO (BASE 2 E BASE 10)
+log10(100)
+log2(8)
+
+#EXPONENCIAL
+exp(1)
+exp(2)
+exp(-2)
+
+#LOGARITMO NATURAL
+log(2.718281828)
+log(100)
+
+#CONSTANTE PI
+pi
+
+#pi radianos = 180 graus
+
+#FUNÇÕES TRIGNOMETRICAS (RADIANOS)
+sin(pi/6) #pi/6 radianos é igual a 30 graus
+asin(0.5)
+
+cos(pi/4) #pi/4 radianos é igual a 45 graus
+acos(0.7071068)
+
+tan(pi/4)
+atan(1)
+
+#NOTAÇÃO CIENTIFICA
+2e32
+2e3
+2e-26
+2e5 + 4e5
+3e20 * 2e12
+15e16 / 5e6
+getRversion() #Saber a versão do R
+
+
+
