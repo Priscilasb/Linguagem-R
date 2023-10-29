eeb98c873821466d64b920aa8c3c279fbe1dccdc
Priscila dos Santos Benedito <90283421+Priscilasb@users.noreply.github.com>
2023-10-29 03:04:57 -0300

    Add files via upload

---
+++ pacotes.R
@@ -0,0 +1,22 @@
+# http://cran.rstudio.com/
+
+# BAIXAR PACOTES, CASO ELES AINDA NÃO ESTEJAM BAIXADOS
+install.packages("argo")
+
+# CARREGAR PACOTES
+library(argo)
+
+?argo
+
+# BAIXAR PACOTES, CASO ELES AINDA NãO ESTEJAM BAIXADOS
+if(!require(argo)) install.packages("argo") 
+
+# CARREGAR PACOTES
+library(argo)
+
+# REMOVER PACOTES
+remove.packages("argo")
+
+
+
+
